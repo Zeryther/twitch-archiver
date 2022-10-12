994a00aeaeb444b4122237999d9f1f0bd475aa54
Mehdi Baaboura <mbaaboura@gigadrivegroup.com>
2022-10-12 20:45:06 +0200

    Updated ffmpeg path

--- run.sh
+++ run.sh
@@ -47,23 +47,37 @@ if [ -z "$videoid" ]; then
 fi
 
 # execute TwitchDownloaderCLI with videoid
-echo SEPARATOR
+echo $SEPARATOR
 echo "Downloading video..."
-echo SEPARATOR
+echo $SEPARATOR
 
-./TwitchDownloaderCLI -m VideoDownload --id $videoid --ffmpeg-path /usr/local/bin/ffmpeg -o $OUTPUT_DIR/$videoid.mp4
+./TwitchDownloaderCLI -m VideoDownload --id $videoid -o $OUTPUT_DIR/$videoid.mp4
 
 if [ "$nochat" == false ]; then
-    echo SEPARATOR
+    echo $SEPARATOR
     echo "Downloading chat..."
-    echo SEPARATOR
+    echo $SEPARATOR
 
     ./TwitchDownloaderCLI -m ChatDownload --id $videoid -o $OUTPUT_DIR/$videoid_chat.json
+
+    echo $SEPARATOR
+    echo "Rendering chat..."
+    echo $SEPARATOR
+
     ./TwitchDownloaderCLI -m ChatRender -i $OUTPUT_DIR/$videoid_chat.json -h 1080 -w 422 --framerate 30 --update-rate 0 --font-size 18 -o $OUTPUT_DIR/$videoid_chat.mp4
+
+    echo $SEPARATOR
+    echo "Combining chat and video..."
+    echo $SEPARATOR
+
     ffmpeg -i $OUTPUT_DIR/$videoid.mp4 -i $OUTPUT_DIR/$videoid_chat.mp4 -filter_complex "[0:v]scale=1498:-1,pad=1498:1080:0:120:black[first];[first][1:v]hstack[stack]" -r 60 -map "[stack]" -map 0:a $OUTPUT_DIR/$videoid_combine.mp4
 
+    echo $SEPARATOR
+    echo "Cleanup..."
+    echo $SEPARATOR
+
     # Cleanup
-    rm $OUTPUT_DIR/$videoid.mp4
-    rm $OUTPUT_DIR/$videoid_chat.mp4
-    rm $OUTPUT_DIR/$videoid_chat.json
+    rm -vrf $OUTPUT_DIR/$videoid.mp4
+    rm -vrf $OUTPUT_DIR/$videoid_chat.mp4
+    rm -vrf $OUTPUT_DIR/$videoid_chat.json
 fi
